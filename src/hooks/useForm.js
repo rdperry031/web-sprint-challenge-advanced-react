baf65ba8190d252ad8879aa21a98c8344da12be1
rdperry031 <rdperry031@gmail.com>
2021-10-01 08:55:52 -0500

    useForm completed

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,8 @@
 // write your custom hook here to control your checkout form
+import { useState } from 'react'
+
+export const useForm = (initialValue) => {
+    const [value, setValue] = useState(initialValue)
+
+    return [value, setValue]
+}
